ee7c4d54cd6421a7c2947ae8b54404b2db372d66
ng-life <2994263+ng-life@users.noreply.github.com>
2025-08-29 18:30:53 +0800

    Add files via upload

---
+++ Casks/orbstack@1.rb
@@ -0,0 +1,52 @@
+cask "orbstack@1" do
+  arch arm: "arm64", intel: "amd64"
+
+  version "1.11.3,19358"
+  sha256 arm:   "ff3ba392672d31d2549b777b4638de89cf10aef592944014863160a2e05778dc",
+         intel: "a0b39329e90353cf34a7d0edcdc47f9aec333ee140defbbbf3d68460d15b731f"
+
+  url "https://cdn-updates.orbstack.dev/#{arch}/OrbStack_v#{version.csv.first}_#{version.csv.second}_#{arch}.dmg"
+  name "OrbStack"
+  desc "Replacement for Docker Desktop"
+  homepage "https://orbstack.dev/"
+
+  livecheck do
+    url "https://cdn-updates.orbstack.dev/#{arch}/appcast.new.xml"
+    strategy :sparkle do |items|
+      items.find { |item| item.channel == "stable" }&.nice_version
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+  app "OrbStack.app"
+  binary "#{appdir}/OrbStack.app/Contents/MacOS/bin/orb"
+  binary "#{appdir}/OrbStack.app/Contents/MacOS/bin/orbctl"
+
+  postflight do
+    system_command "#{appdir}/OrbStack.app/Contents/MacOS/bin/orbctl",
+                   args: ["_internal", "brew-postflight"]
+  end
+
+  uninstall script: {
+    executable: "#{appdir}/OrbStack.app/Contents/MacOS/bin/orbctl",
+    args:       ["_internal", "brew-uninstall"],
+  }
+
+  zap trash: [
+        "~/.orbstack",
+        "~/Library/Caches/dev.kdrag0n.MacVirt",
+        "~/Library/Group Containers/*.dev.orbstack",
+        "~/Library/HTTPStorages/dev.kdrag0n.MacVirt",
+        "~/Library/HTTPStorages/dev.kdrag0n.MacVirt.binarycookies",
+        "~/Library/Preferences/dev.kdrag0n.MacVirt.plist",
+        "~/Library/Saved Application State/dev.kdrag0n.MacVirt.savedState",
+        "~/Library/WebKit/dev.kdrag0n.MacVirt",
+      ],
+      rmdir: "~/OrbStack"
+
+  caveats <<~EOS
+    Open the OrbStack app to finish setup.
+  EOS
+end
