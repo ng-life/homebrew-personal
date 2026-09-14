33b609844aaf94e1db94a4811f41bfb8cd753548
lifeng <lifeng@yijiupi.com>
2026-09-14 12:52:07 +0800

    SYS-N32132 添加 ripgrep-web formula 同步

---
+++ Formula/ripgrep-web.rb
@@ -0,0 +1,40 @@
+class RipgrepWeb < Formula
+  desc "Lightweight in-process ripgrep web log search service"
+  homepage "https://github.com/ng-life/ripgrep-web"
+  version "0.0.1"
+  license "MIT"
+
+  on_macos do
+    url "https://github.com/ng-life/ripgrep-web/releases/download/v0.0.1/ripgrep-web-v0.0.1-aarch64-apple-darwin.tar.gz"
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  end
+
+  on_linux do
+    url "https://github.com/ng-life/ripgrep-web/releases/download/v0.0.1/ripgrep-web-v0.0.1-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  end
+
+  def install
+    (var/"log/ripgrep-web").mkpath
+    bin.install "ripgrep-web"
+  end
+
+  service do
+    run [opt_bin/"ripgrep-web"]
+    environment_variables LOG_BASE_DIR: var/"log/ripgrep-web",
+                          LISTEN_ADDR: "127.0.0.1:5000",
+                          MAX_CONCURRENT_SEARCHES: "4",
+                          RUST_LOG: "ripgrep_web=info,tower_http=info"
+    keep_alive true
+    log_path var/"log/ripgrep-web.log"
+    error_log_path var/"log/ripgrep-web.log"
+  end
+
+  def caveats
+    <<~EOS
+      The service defaults to searching #{var}/log/ripgrep-web.
+      Start it with:
+        brew services start #{tap}/ripgrep-web
+    EOS
+  end
+end
